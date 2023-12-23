1f8a6e5b356c27ccd8f34e633d67768f12b5c780
rasvanjaya21 <mrsejati17@gmail.com>
2023-12-23 21:24:54 +0700

    init: express

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+	res.send({ message: "Welcome to IIKA API Service" });
+});
+
+app.get("/api/services", (req, res) => {
+	res.send({
+		id: 1,
+		product_code: 2023,
+		product_name: "Consultant",
+		product_description: "Consultant is an experienced professional with a broad and in-depth knowledge of a particular subject matter who advises clients.",
+		product_image: "https://en.wikipedia.org/static/images/icons/wikipedia.png",
+	});
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
